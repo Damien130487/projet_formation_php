83f296fe133062cdb05b1b4f891bd66fb281045d
damien <quesnel.damien13@outlook.fr>
2025-04-14 13:43:01 +0200

    mise en place du serveur

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="base.css" />
+    <title>CV</title>
+  </head>
+
+  <body>
+     
+
+    tutu
+  </body>
+</html>
